081fcc5e2d46f68276f8457460a30416359c27f0
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-06-11 22:39:43 -0700

    Fix imports

--- twogather_frontend/src/components/searchBar/SearchBar.js
+++ twogather_frontend/src/components/searchBar/SearchBar.js
@@ -2,7 +2,7 @@ import { React, useState } from "react";
 import TextField from "@mui/material/TextField";
 // import List from "./List";
 import "./searchbar.css";
-import TagsInput from "../Tags/TagsInput";
+import TagsInput from "../tags/TagsInput";
 
 function SearchBar() {
   const [inputText, setInputText] = useState("");

fb09b2dba82a5f42a3855f54d83d8f5d06d58827
Brayden Tam <brayden.tam@gmail.com>
2022-06-11 17:37:17 -0700

    maps in progrses

--- twogather_frontend/src/App.js
+++ twogather_frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/home.js';
 import Profile from './pages/profile/profile.js';
-import Navbar from './components/navbar/index.js';
+import Navbar from './components/navbar/Navbar.js';
 import Signup from './pages/signup/signup.js';
 import Events from './pages/events/events.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';

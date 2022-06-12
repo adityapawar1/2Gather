344df635b2243397ba8986c9e7a8164d9845919c
vivek nadig <vivek.nadig@warriorlife.net>
2022-06-11 17:36:37 -0700

    Fixed bugs in EventMap.js

--- twogather_frontend/src/App.js
+++ twogather_frontend/src/App.js
@@ -1,21 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './pages/home/home.js';
-import Profile from './pages/profile/profile.js';
-import Navbar from './components/navbar/index.js';
-import Signup from './pages/signup/signup.js';
-import Events from './pages/events/events.js';
+import Home from './pages/home/Home.js';
+import Profile from './pages/profile/Profile';
+import NavBar from './components/navbar/NavBar';
+import Signup from './pages/signup/Signup';
+import Events from './pages/events/Events';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar />
+      <NavBar/>
       <Routes>
         <Route exact path="/home" element={<Home/>} />
         <Route exact path="/profile" element={<Events/>} />
-        
       </Routes>
     </Router>
     </div>
--- twogather_frontend/src/components/eventMap/EventMap.js
+++ twogather_frontend/src/components/eventMap/EventMap.js
@@ -17,7 +17,7 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          bootstrapURLKeys={{ key: ''}}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >

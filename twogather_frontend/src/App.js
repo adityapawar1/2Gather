a8f653cfde0a5132f5358a446ee402dbae5274ae
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-06-12 10:41:25 -0700

    ALmost done with chat

--- twogather_frontend/src/App.js
+++ twogather_frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/home/Home.js";
-import Profile from "./pages/profile/profile.js";
+import Profile from "./pages/profile/Profile.js";
 import NavBar from "./components/navbar/NavBar.js";
 import SearchBar from "./components/searchBar/SearchBar.js";
 import Signup from "./pages/signup/Signup.js";
@@ -9,12 +9,17 @@ import Events from "./pages/events/Events.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Chat from "./components/chat/Chat.tsx";
 import UserContext from "./store";
-import { SocketProvider } from "./SocketContext";
+import { SocketProvider } from "@ericlathrop/phoenix-js-react-hooks";
 import Event from "./components/Events/Event";
 
 function App() {
   const socketUrl = "ws://localhost:4000/socket";
-  const socketOptions = { params: { token: "" } };
+  const socketOptions = {
+    // logger: (kind, msg, data) => {
+    //   console.log(`${kind}: ${msg}`, data);
+    // },
+    params: { token: "dasdsadasdasdasd" },
+  };
 
   return (
     <div className="App">
@@ -34,7 +39,7 @@ function App() {
         {/* <Routes> */}
         {/* <Route exact path="/home" element={<Home/>} />
         <Route exact path="/profile" element={<Events/>} /> */}
-        <Events />
+        {/* <Events /> */}
         {/* </Routes>
     </Router> */}
         {/* <SearchBar /> */}
--- twogather_frontend/src/components/chat/Chat.tsx
+++ twogather_frontend/src/components/chat/Chat.tsx
@@ -3,8 +3,11 @@ import UserContext from "../../store";
 import "./Chat.css";
 import { Socket } from "phoenixjs";
 import ChatMessage from "./ChatMessage.tsx";
-import { useEventHandler } from "@ericlathrop/phoenix-js-react-hooks";
-import { SocketContext } from "../../SocketContext";
+import {
+  sendMessage,
+  useChannel,
+  useEventHandler,
+} from "@ericlathrop/phoenix-js-react-hooks";
 
 type ChatProps = {
   event_id: number | null;
@@ -51,14 +54,11 @@ export default function Chat({ event_id }: ChatProps) {
   const [messages, setMessages] = useState<Message[]>(testMessages);
   const { user_name, jwt_token }: UserContextType = useContext(UserContext);
 
-  const { channel: chatChannel }: any = useChannel(
+  const channel: any = useChannel(
     "event:" + event_id,
     { token: jwt_token },
-    (channel, { messages: initialMessages }) => {
-      setMessages(initialMessages);
-    }
+    (x, y) => {}
   );
-
   // useEventHandler(chatChannel, "new_msg", (message) => {
   //   console.log("new message", message);
   //   setMessages(messages.slice(0).concat([message]));
@@ -67,11 +67,12 @@ export default function Chat({ event_id }: ChatProps) {
   return (
     <div className="w-1/5 m-auto my-2">
       <div className="flex flex-col overflow-auto h-screen">
-        {messages.map((message: Message): ReactNode => {
+        {messages.map((message: Message, index: number): ReactNode => {
           return (
             <ChatMessage
               user={message.sender}
               body={message.body}
+              id={index}
               sentByCurrentUser={user_name == message.sender}
             />
           );
@@ -84,54 +85,4 @@ export default function Chat({ event_id }: ChatProps) {
       />
     </div>
   );
-}
-
-function useChannel(topic, params, onJoin) {
-  console.log("INSIDE THE HOOK");
-  const { socket } = useContext(SocketContext);
-  const [channel, setChannel] = useState(null);
-
-  const onJoinFun = useRef(onJoin);
-
-  useEffect(() => {
-    if (onJoinFun) {
-      onJoinFun.current = onJoin;
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log("SOMETHING BIG HAPPENED");
-    if (socket === null) {
-      console.log("socket is null");
-      return;
-    }
-    const ch = socket.channel(topic, params);
-    ch.join().receive("ok", (message) => onJoinFun.current(ch, message));
-    setChannel(ch);
-
-    return () => {
-      console.log("unsubscribing from channel");
-      ch.leave();
-      setChannel(null);
-    };
-  }, [socket, topic, params]);
-
-  console.log("SOCKET", socket);
-  console.log("THIS IS THE CHANNEL");
-  console.log(channel);
-  return channel;
-}
-
-function pushPromise(push) {
-  return new Promise((resolve, reject) => {
-    if (!push) {
-      return reject("no push");
-    }
-    push.receive("ok", resolve).receive("error", reject);
-    // .receive('timeout', reject('timeout'));
-  });
-}
-
-function sendMessage(channel, event, payload) {
-  return pushPromise(channel.push(event, payload));
 }

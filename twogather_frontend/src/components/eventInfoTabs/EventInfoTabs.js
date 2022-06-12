9d00b30b7dba7bf525e7dd1f46b131c14c83d783
Brayden Tam <brayden.tam@gmail.com>
2022-06-12 12:39:56 -0700

    events frontend logic-- needs connecting

--- twogather_frontend/src/components/eventInfoTabs/EventInfoTabs.js
+++ twogather_frontend/src/components/eventInfoTabs/EventInfoTabs.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import './EventInfoTabs.css'
 import { Container, Row, Col } from 'react-bootstrap';
-const EventInfoTabs = () => {
+import events from './events.json';
+
+class EventInfoTabs extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            eventJson: events.events,
+            event_id: 4
+        }
+    }
+    render() {
   return (
     <div>
         <Container>
@@ -11,22 +21,25 @@ const EventInfoTabs = () => {
                 </div>    
                 <div id="info">
                     <br></br>
-                    05 June 2021 5PM
+                    {this.state.eventJson[this.state.event_id].time['start']}
+                    {/* {$.getJSON(locationstring, function (result) { 
+                        console.log(eventJson[0].time['start'])} */}
                 </div></Col>
                 <Col className="column"><div id="title">
                     End Time
                 </div>    
                 <div id="info">
                     <br></br>
-                    05 June 2021 6PM
+                    {this.state.eventJson[this.state.event_id].time['end']}
                 </div></Col>
                 <Col className="column"><div id="title">
                     Number of Attendees
+                    
                 </div>    
                 <div id="info">
                     <br></br>
                     
-                    6 People
+                    {this.state.eventJson[this.state.event_id].participants.length} People
                 </div></Col>
                 {/* <Col className="hiddenColumn">    </Col> */}
                 <Col>
@@ -38,6 +51,7 @@ const EventInfoTabs = () => {
         
     </div>
   )
+  }
 }
 
 export default EventInfoTabs;

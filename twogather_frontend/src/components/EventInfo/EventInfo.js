05d6ac19a90d88c48fc161a17dadff8144e9cafd
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-06-11 19:59:56 -0700

    Rename files

--- twogather_frontend/src/components/EventInfo/EventInfo.js
+++ twogather_frontend/src/components/EventInfo/EventInfo.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import './EventInfo.css';
-import Home from '../eventMap/EventMap';
-import EventInfoTabs from '../eventInfoTabs/EventInfoTabs';
-
+import React from "react";
+import "./EventInfo.css";
+import Home from "../eventMap/EventMap";
+import EventInfoTabs from "../eventInfoTabs/EventInfoTabs";
 
 class EventInfo extends React.Component {
-    constructor(props) {
-        super(props);
-    
-    }
-    
-    render() {
-        
-        return (
-            <div>
-                <div id="top">
-                    <div id="name">Bowling with Vivek</div>
-                    <div id="host">Created by vivek.nadig@warriorlife.net</div>
-                    <button className="button" id="register">Register</button>
-                </div>
-                <div id="map" className="map">
-                 <Home/>
-                
-                </div>
-                <div id = "bottom">
-                    <div className="tabs">
-                        <EventInfoTabs/>
-                    </div>
-                  
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="top">
+          <div id="name">Bowling with Vivek</div>
+          <div id="host">Created by vivek.nadig@warriorlife.net</div>
+          <button className="button" id="register">
+            Register
+          </button>
+        </div>
+        <div id="map" className="map">
+          <Home />
+        </div>
+        <div id="bottom">
+          <div className="tabs">
+            <EventInfoTabs />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default EventInfo;

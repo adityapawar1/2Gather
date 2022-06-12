a08e5862daf877f346326f20067f41d1e507e650
vivek nadig <vivek.nadig@warriorlife.net>
2022-06-11 20:40:31 -0700

    Added styling to tag input

--- twogather_frontend/src/components/Tags/TagsInput.js
+++ twogather_frontend/src/components/Tags/TagsInput.js
@@ -16,10 +16,10 @@ function TagsInput(){
     }
 
     return (
-        <div className="tags-input-container p-2">
+        <div className="p-2">
             { tags.map((tag, index) => (
-                <div className="tag-item" key={index}>
-                    <span className="text">{tag}</span>
+                <div className="tag-item m-1" key={index}>
+                    <span className="ml-1 text">{tag}</span>
                     <span className="close" onClick={() => removeTag(index)}>&times;</span>
                 </div>
             )) }

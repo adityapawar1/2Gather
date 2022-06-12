b6f07d6f76925c7d1ef9dfdeb1510961364310d2
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-06-11 20:26:34 -0700

    Fix casing for imports

--- twogather_frontend/src/components/User/User.js
+++ twogather_frontend/src/components/User/User.js
@@ -1,24 +1,20 @@
-import React from 'react'
-class User extends React.Component{
-    constructor(props){
-        let tag_list = [];// backend call to get list of activities
-        super(props);
-    }
-    render(){
-        return(
-            <div>
-                <Col>
-                    <Row>
-                        <h1>
-                            Welcome {this.props.username}
-                        </h1>
-                    </Row>
-                    <Row>
-
-                    </Row>
-                </Col>
-            </div>
-        )
-    }
+import React from "react";
+class User extends React.Component {
+  constructor(props) {
+    let tag_list = []; // backend call to get list of activities
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Col>
+          <Row>
+            <h1>Welcome {this.props.username}</h1>
+          </Row>
+          <Row></Row>
+        </Col>
+      </div>
+    );
+  }
 }
 export default User;
